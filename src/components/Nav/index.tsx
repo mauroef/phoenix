280ab3d67742a4f2649e03eb736fd9ae0d991a86
mauroef <cypress90@gmail.com>
2022-04-06 19:31:19 -0300

    Restyling top navbar drunked.

--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -2,24 +2,25 @@ import React, { FC } from 'react'
 import { Link } from 'gatsby'
 import { NavStyled } from './styles'
 
-
 const TopNav: FC = () => {
   return (
     <NavStyled>
       <div>
-        <span>Portfolio</span>
+        <div>
+          <span>Portfolio</span>
+        </div>
+        <ul>
+          <li>
+            <Link to='/'>Home</Link>
+          </li>
+          <li>
+            <Link to='/about'>About Me</Link>
+          </li>
+          <li>
+            <Link to='/projects'>Projects</Link>
+          </li>
+        </ul>
       </div>
-      <ul>
-        <li>
-          <Link to='/' className='blue'>Home</Link>
-        </li>
-        <li>
-          <Link to='/about' className='green'>About Me</Link>
-        </li>
-        <li>
-          <Link to='/projects' className='red'>Projects</Link>
-        </li>
-      </ul>
     </NavStyled>
   )
 }
--- src/components/Nav/styles.ts
+++ src/components/Nav/styles.ts
@@ -1,77 +1,67 @@
 import styled from '@emotion/styled'
 
 export const NavStyled = styled.nav`
-  align-items: center;
   backdrop-filter: blur(0.125rem);
   background: rgba(9, 18, 37, 0.4);
-  display: flex;
   height: 3.5rem;
-  justify-content: space-between;
   padding: 0 1rem;
   position: fixed;
   top: 0;
   width: 100%;
-  div {
-    display: none;
-  }
-  ul {
-    list-style-type: none;
+  > div {
+    align-items: center;
     display: flex;
     justify-content: space-between;
-    width: 100%;
-    & > li {
-      & > a {
-        background-color: transparent;
-        display: inline-block;
-        text-decoration: none;
-        font-size: 0.875rem;
-        font-weight: 500;
-        padding: 0.4688rem 1.25rem;
-        border: 0.0625rem solid;
-        border-radius: 0.375rem;
-        &.red {
-          border-color: #FF2D55;
-          color: #FF2D55;
+    margin: 0 auto;
+    height: 100%;
+    div {
+      display: none;
+    }
+    ul {
+      list-style-type: none;
+      display: flex;
+      justify-content: space-between;
+      width: 100%;
+      & > li {
+        & > a {
+          background-color: transparent;
+          display: inline-block;
+          text-decoration: none;
+          font-size: 0.875rem;
+          font-weight: 400;
+          padding: 0.4688rem 1.25rem;
+          border: 0.0625rem solid;
+          border-radius: 0.375rem;
+          border-color: #fff;
+          color: #fff;
           &:hover {
-            background-color: #FF2D55;
-            color: #FFF;
+            background-color: #1d1d1f;
+            border-color: #1d1d1f;#
+            color: #fff;
+            font-weight: 600;
+            transition: 0.3s;
           }
-        }
-        &.green {
-          border-color: #34c759;
-          color: #34c759;
-          &:hover {
-            background-color: #34c759;
-            color: #FFF;
-          }
-        }
-        &.blue {
-          border-color: #007aff;
-          color: #007aff;
-          &:hover {
-            background-color: #007aff;
-            color: #FFF;
-          }
-        }
-        &:hover {
-          transition: 0.3s;
         }
       }
     }
   }
   @media (min-width: 40rem) {
-    div {
-      display: block;
-      > span {
-        color: #FFF;
-        font-weight: 500;
+    > div {
+      max-width: 75rem;
+      div {
+        display: block;
+        > span {
+          color: #fff;
+          font-size: 1.3rem;
+          font-weight: 500;
+        }
       }
-    }
-    ul {
-      justify-content: flex-end;
-      li:nth-of-type(1),
-      li:nth-of-type(2) {
-        margin-right: 1rem;
+      > ul {
+        justify-content: flex-end;
+        li:nth-of-type(1),
+        li:nth-of-type(2) {
+          margin-right: 1rem;
+        }
       }
     }
   }
