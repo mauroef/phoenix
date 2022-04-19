f5b2e65d5127e2b571cfdc8ec708e5145cd08d2b
mauroef <cypress90@gmail.com>
2022-04-19 01:16:44 -0300

    Using reveal.

--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -1,4 +1,5 @@
 import React, { FC } from 'react'
+import { Fade } from 'react-awesome-reveal'
 import { ListStyled } from './styles'
 import Card from '../Card/'
 import { Node } from '../../interfaces'
@@ -14,17 +15,19 @@ interface ListProps {
 const List: FC<ListProps> = ({ items }) => {
   return (
     <ListStyled>
-      {items.allMdx.nodes.map((node: Node) => (
-        <Card
-          key={node.id}
-          title={node.frontmatter.title || ''}
-          description={node.frontmatter.description}
-          demo={node.frontmatter.demo || ''}
-          repo={node.frontmatter.repo || ''}
-          stack={node.frontmatter.stack || []}
-          image={node.frontmatter.image}
-        />
-      ))}
+      <Fade cascade direction='up' fraction={0.6} triggerOnce>
+        {items.allMdx.nodes.map((node: Node) => (
+          <Card
+            key={node.id}
+            title={node.frontmatter.title || ''}
+            description={node.frontmatter.description}
+            demo={node.frontmatter.demo || ''}
+            repo={node.frontmatter.repo || ''}
+            stack={node.frontmatter.stack || []}
+            image={node.frontmatter.image}
+          />
+        ))}
+      </Fade>
     </ListStyled>
   )
 }
