c77663da32f46123f95ca9690a8836555e65351b
mauroef <cypress90@gmail.com>
2022-04-25 22:42:48 -0300

    Bottom navigator on mobile.

--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -1,6 +1,7 @@
 import React, { FC } from 'react'
 import { Link } from 'gatsby'
 import { NavStyled } from './styles'
+import { Briefcase, Home, User } from '@emotion-icons/typicons'
 
 const TopNav: FC = () => {
   return (
@@ -11,13 +12,22 @@ const TopNav: FC = () => {
         </div>
         <ul>
           <li>
-            <Link to='/'>Home</Link>
+            <Link to='/'>
+              <Home title='home' size='24' />
+              Home
+            </Link>
           </li>
           <li>
-            <Link to='/about'>About Me</Link>
+            <Link to='/about'>
+              <User title='about' size='24' />
+              About Me
+            </Link>
           </li>
           <li>
-            <Link to='/projects'>Projects</Link>
+            <Link to='/projects'>
+              <Briefcase title='projects' size='24' />
+              Projects
+            </Link>
           </li>
         </ul>
       </div>
